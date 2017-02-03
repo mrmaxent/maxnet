631eee32ff55e72f453d7f27601d94b3940ec21f
mrmaxent <mrmaxent@gmail.com>
2017-02-03 08:29:59 -0700

    Add check that glmnet was successful
    Added check that glmnet computed the entire regularization path.  Also
    increased the number of lambda values from 100 to 200.  Reason: on some
    of the NCEAS dataset, glmnet didn't converge, and returned less than the
    requested number of beta values.

--- R/maxnet.R
+++ R/maxnet.R
@@ -8,9 +8,10 @@ function(p, data, f=maxnet.formula(p, data), regmult=1.0,
    reg <- regfun(p,mm) * regmult
    weights <- p+(1-p)*100
    glmnet::glmnet.control(pmin=1.0e-8, fdev=0)  
-   model <- glmnet::glmnet(x=mm, y=as.factor(p), family="binomial", standardize=F, penalty.factor=reg, lambda=10^(seq(4,0,length.out=100))*sum(reg)/length(reg)*sum(p)/sum(weights), weights=weights, ...)
+   model <- glmnet::glmnet(x=mm, y=as.factor(p), family="binomial", standardize=F, penalty.factor=reg, lambda=10^(seq(4,0,length.out=200))*sum(reg)/length(reg)*sum(p)/sum(weights), weights=weights, ...)
    class(model) <- c("maxnet", class(model))
-   bb <- model$beta[,100]
+   if (length(model$beta) < 200) stop("Error: glmnet failed to complete regularization path")
+   bb <- model$beta[,200]
    model$betas <- bb[bb!=0]
    model$alpha <- 0
    rr <- predict.maxnet(model, data[p==0, , drop = FALSE], type="exponent", clamp=F)
