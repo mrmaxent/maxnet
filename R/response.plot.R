1fc0bfef966f0a6d43afb4e79e596691c7eeb6e3
mrmaxent <mrmaxent@gmail.com>
2020-09-22 20:03:22 -0600

    Update response.plot.R
    Allow response.plot to pass additional arguments to plot and barplot.

--- R/response.plot.R
+++ R/response.plot.R
@@ -1,19 +1,17 @@
 #' @export
 response.plot <-
-function(mod, v, type, mm=mod$samplemeans, min=mod$varmin[v], max=mod$varmax[v], levels=unlist(mod$levels[v]), plot=T, ylim=NULL, ylab=NULL) {
+function(mod, v, type, mm=mod$samplemeans, min=mod$varmin[v], max=mod$varmax[v], levels=unlist(mod$levels[v]), plot=T, xlab=v, ylab=tools::toTitleCase(type), ...) {
    nr <- if (is.null(levels)) 100 else length(levels)
    m <- data.frame(matrix(mm,nr,length(mm),byrow=T))
    colnames(m) <- names(mm)
    m[,v] <- if (!is.null(levels)) levels else 
       seq(min - 0.1*(max-min), max+0.1*(max-min), length=100)
    preds <- predict(mod, m, type=type)
-   if (is.null(ylab))
-      ylab <- paste(toupper(substring(type,1,1)), substring(type, 2), sep="")
    if (plot) {
       if (is.null(levels)) {
-         plot(m[,v], preds, xlab=v, ylab=ylab, type="l", ylim=ylim) 
+         plot(m[,v], preds, xlab=xlab, ylab=ylab, type="l", ...) 
       } else {
-         graphics::barplot(as.vector(preds), names.arg=levels, xlab=v, ylab=ylab, ylim=ylim)
+         graphics::barplot(as.vector(preds), names.arg=levels, xlab=xlab, ylab=ylab, ...)
       }
    }
    else return(setNames(data.frame(m[, v], preds), c(v, 'pred')))
