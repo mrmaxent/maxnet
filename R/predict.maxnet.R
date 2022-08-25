4a317587cd80bcfcbd7dcb315e9fef0740a5ac8f
Ben Tupper <btupper@bigelow.org>
2022-08-25 11:30:49 -0400

    add N and mar to plot, remove response.R, finalize notebook

--- R/predict.maxnet.R
+++ R/predict.maxnet.R
@@ -1,7 +1,41 @@
+#' Predict using a maxnet model
+#' 
 #' @export
+#' @param object an object of class "maxnet", i.e., a fitted model.
+#' @param newdata values of predictor variables to predict to, possibly
+#'   matrix, data.frame or \code{stars} object
+#' @param clamp logical, f true, predictors and features are restricted to the range seen during model training.
+#' @param type character, type of response required. Using \code{lp} for the linear predictor 
+#' and \code{entropy} for the entropy of the exponential model over the background data, 
+#' the values returned are determined by the value of \code{type}.
+#' \itemize{
+#'   \item{"link"}{yields \code{lp}}
+#'   \item{"exponential"}{yields \code{exp(lp)}}
+#'   \item{"cloglog"}{yields \code{1-exp(-exp(entropy+lp))}}
+#'   \item{"logistic"}{yields \code{1/(1+exp(-entropy-lp))}}
+#' }
+#' @param ... not used
+#' @return vector with predicted values (one per input row) or \code{stars} object of predicted values
 predict.maxnet <-
 function(object, newdata, clamp=T, type=c("link","exponential","cloglog","logistic"), ...)
 {
+   is_stars <- inherits(newdata, "stars")
+   if (is_stars){
+     if (!requireNamespace("stars", quietly = TRUE)) {
+       stop("package stars required, please install it first")
+     }
+     S <- newdata
+     newdata <- as.data.frame(S)
+     newdata <- newdata[ , -c(1,2)] # drop x,y leading columns
+     ix <- complete.cases(newdata)
+     newdata <- newdata[ix, ]
+     # slice out just the first attribute - a copy,
+     # make all of it's values NA
+     S <- S[1]
+     names(S) <- "pred"
+     S$pred[] <- NA_real_
+   }  
+  
    if (clamp) {
       for (v in intersect(names(object$varmax), names(newdata))) {
          newdata[,v] <- pmin(pmax(newdata[,v], object$varmin[v]), object$varmax[v])
@@ -16,8 +50,15 @@ function(object, newdata, clamp=T, type=c("link","exponential","cloglog","logist
                  object$featuremaxs[names(object$betas)]))
    link <- (mm %*% object$betas) + object$alpha
    type <- match.arg(type)
-   if (type=="link") return(link)
-   if (type=="exponential") return(exp(link))
-   if (type=="cloglog") return(1-exp(0-exp(object$entropy+link)))
-   if (type=="logistic") return(1/(1+exp(-object$entropy-link)))
+   r <- switch(tolower(type[1]),
+     "exponential" = exp(link),
+     "cloglog" = 1-exp(0-exp(object$entropy+link)),
+     "logistic"= 1/(1+exp(-object$entropy-link)),
+     link)
+
+   if (is_stars) {
+     S$pred[ix] <- r 
+     r <- S
+   }
+   return(r)
 }
