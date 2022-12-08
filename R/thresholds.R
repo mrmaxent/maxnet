0ff7a3b4805096be48e7506cff99d80b4282bdeb
mrmaxent <mrmaxent@gmail.com>
2022-12-08 10:38:05 -0700

    Update thresholds.R to allow for a knot at the second smallest value
    Previously the smallest allowed knot was at the third smallest value, now the second smallest is allowed too.

--- R/thresholds.R
+++ R/thresholds.R
@@ -5,7 +5,7 @@ function(x, nknots=50)
 {
    min <- min(x)
    max <- max(x)
-   k <- seq(min, max, length=nknots+2)[2:nknots+1]
+   k <- seq(min, max, length=nknots+2)[2:(nknots+1)]
    f <- outer(x, k, function(w,t) ifelse(w>=t,1,0))
    colnames(f) <- paste("",k, sep=":")
    f
